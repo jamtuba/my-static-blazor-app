4cd9a286273c3d2f384a3a690c0cdf859cf79dd5
jamtuba <jamtuba@gmail.com>
2021-02-14 14:37:06 +0100

    Added a ProductsGet Added a launchSettings

---
+++ Api/ProductsGet.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Data;
+
+namespace Api
+{
+    public class ProductsGet
+    {
+        private readonly IProductData _productData;
+
+        public ProductsGet(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        [FunctionName("ProductsGet")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"Getting with request: {req.Body}");
+
+            var products = await _productData.GetProducts();
+            return new OkObjectResult(products);
+        }
+    }
+}
