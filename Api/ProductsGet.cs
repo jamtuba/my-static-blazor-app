3ccd7c80a53d5b1a29f5a7eba6c895edeac05877
jamtuba <jamtuba@gmail.com>
2021-02-14 14:48:03 +0100

    Ups, forgot the route in ProductsGet

--- Api/ProductsGet.cs
+++ Api/ProductsGet.cs
@@ -22,7 +22,7 @@ namespace Api
 
         [FunctionName("ProductsGet")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation($"Getting with request: {req.Body}");
